118cb75c00ee9b55207a126bd599cf4ad098c261
samuelail <ailemenizzy@gmail.com>
2023-09-22 20:55:54 -0400

    Added toast

---
+++ StackUIKit/Classes/Views/ToastView.swift
@@ -0,0 +1,20 @@
+//
+//  ToastView.swift
+//  StackUIKit
+//
+//  Created by samuel Ailemen on 9/18/23.
+//
+
+import SwiftUI
+
+struct ToastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToastView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToastView()
+    }
+}
